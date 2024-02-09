2d4de0c342d2af68a8bbc0d278814a5f51acb19a
Robin Ray <robin.ray@crayon.co>
2024-02-09 07:51:30 -0600

    change post column types

--- app/models.py
+++ app/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, db_index=True)
-    content = models.TextField(db_index=True)
+    title = models.TextField(db_index=True)
+    content = models.CharField(max_length=100, db_index=True)
